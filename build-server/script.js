3265c7bf898963abdf343df1b9956b62461d15de
installer <syedabuubaid@gmail.com>
2025-03-27 02:43:05 +0530

    init aws S3

--- build-server/script.js
+++ build-server/script.js
@@ -1,23 +1,7 @@
 const { exec } =require("child_process")
 const { path } = require("path" )
+import {S3Client,PutObjectCommand} from '@aws-sdk/client-s3'
 
-
-// do i need to do here?? 
-// docker makes an isolated container 
-// afer that main.sh script is triggerd
-// we clone the github repo 
-// all of this is done inside the container 
-// now after cloning we will execute a js file and it will npm run build and npm run install
-//done till this step 
-
-
-// we will take the dist folder and upload it to s3 client?
-// so what do i need ???
-// when i first get inside this 
-// i need to know the path of the repo 
-// so we'll get to the path 
-// after that we will build the repo and upload to s3
-// so lets focus on getting the repo path .
 
 async function init(){
     console.log('Genrating...')
