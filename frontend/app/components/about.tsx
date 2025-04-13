675a8735db639a484e183e3d7aa2ded03e8595a1
installer <syedabuubaid@gmail.com>
2025-04-14 01:33:20 +0530

    fix:padding

--- frontend/app/components/about.tsx
+++ frontend/app/components/about.tsx
@@ -1,6 +1,6 @@
 export const About=()=>{
     return(
-        <div className="h-[400px] w-max p-[30px] mt-[50px]">
+        <div className="h-[400px] w-max p-[60px] px-[120px] mt-[50px]">
             <div className="font-bold text-2xl tracking-tight">what is runix?</div>
             <div className="tracking-tighter text-xl">runix allows users to deploy their code seamlessly with minimal time , with efficient workflow and scalable architecture our design handles everything.</div>
         </div>
