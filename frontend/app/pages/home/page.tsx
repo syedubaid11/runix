3f2b8d5b94a3ff6fa3614fa01c65ba5213bb97da
installer <syedabuubaid@gmail.com>
2025-04-14 00:47:04 +0530

    commit

---
+++ frontend/app/pages/home/page.tsx
@@ -0,0 +1,9 @@
+export const HomeSection=()=>{
+    return(
+        <div className="text-4xl font-[40px] tracking-tight absolute top-[200px]">
+            <span className="tracking-tighter text-6xl font-bold">runix</span> ,deploy in seconds
+            <div><button>Link to the repository</button></div>
+        </div>
+    )
+
+}
