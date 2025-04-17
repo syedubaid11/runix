e9af3bc76a2e942840e5b4777093fbd246437117
syedubaid <syedabuubaid@gmail.com>
2025-04-18 00:18:10 +0530

    feat:added toaster

--- frontend/app/pages/home/page.tsx
+++ frontend/app/pages/home/page.tsx
@@ -1,19 +1,29 @@
 "use client"
-
+import { Toaster , toast } from "react-hot-toast";
 import { useState } from "react";
 
 export const HomeSection=()=>{
     const [input,setInput]=useState('');
+
     const handleSubmit=(e:React.FormEvent)=>{
+        if(!input.trim()){
+            toast.error('Empty field!');
+
+        }
+        else{
+            toast.success('Searching...')
+        }
         e.preventDefault();
-        console.log(e.target);
+        console.log(input);
+        setInput('');
     }
     return(
-        <div className="text-4xl font-[40px] tracking-tight">
-            <span className="tracking-tighter text-6xl font-bold">runix</span> ,deploy in seconds
+        <div className="tracking-tight">
+            <span className="tracking-tighter text-[100px] font-bold">runix</span><span className="text-[30px]">   deploy in seconds</span>
             <div className="flex flex-row items-center justify-center mt-[20px]">
-                  <form onSubmit={handleSubmit} className="flex items-center text-2xl" ><input type="text" onChange={(e)=>{setInput(e.target.value)}} placeholder="Enter the Repository Url..."/><button className="ml-[8px] border border-gray-200 text-2xl p-[5px] rounded-md hover:cursor-pointer hover:bg-gray-100 transition-all duration-300" type="submit">Submit</button></form>
+                  <form onSubmit={handleSubmit} className="flex items-center text-2xl" ><input type="text" value={input} onChange={(e)=>{setInput(e.target.value)}} placeholder="Enter the Repository Url..."/><button className="ml-[8px] border border-gray-200 text-2xl p-[5px] rounded-md hover:cursor-pointer hover:bg-gray-100 transition-all duration-300" type="submit">Submit</button></form>
             </div>
+            <Toaster position="bottom-center"/>
         </div>
     )
 
