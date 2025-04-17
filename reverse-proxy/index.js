bad243a2b3c3894eda3d67d1582c05c23a2c79a1
syedubaid <syedabuubaid@gmail.com>
2025-04-17 20:59:15 +0530

    fix:working

--- reverse-proxy/index.js
+++ reverse-proxy/index.js
@@ -4,9 +4,9 @@ const httpProxy=require('http-proxy')
 const app=express();
 const PORT=8000;
 
-const BASE_PATH="https://runix-bucket.s3.ap-south-1.amazonaws.com/__outputs"
+const BASE_PATH="https://runix-v2.s3.ap-south-1.amazonaws.com/__outputs"
 
-const proxy=httpProxy.createProxysys()
+const proxy=httpProxy.createProxy()
 
 app.listen(PORT,()=>{
     console.log('Reverse Proxy listening on PORT:',PORT)
@@ -14,6 +14,7 @@ app.listen(PORT,()=>{
 
 app.use((req,res)=>{
     const hostname=req.hostname;
+    console.log(hostname);
     const subdomain=hostname.split('.')[0];
 
     const resolvesTo=`${BASE_PATH}/${subdomain}`
