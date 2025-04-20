f8c4df89e7471074b734b131112da321c2d8ae78
syedubaid <syedabuubaid@gmail.com>
2025-04-21 01:49:30 +0530

    api server

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import { ECSClient, RunTaskCommand} from '@aws-sdk/client-ecs';
 import 'dotenv/config'
-
+import { z } from 'zod';
 
 const app=express();
 const PORT=9000;
+app.use(express.json());
+
+export const uploadSchema=z.object({
+  git_url:z.string(),
+  project_id:z.string()
+})
+
 
 const client=new ECSClient({
     credentials:{
@@ -13,18 +20,23 @@ const client=new ECSClient({
     }
 }) 
 
-const project_link="https://github.com/syedubaid11/test-project";
+app.post('/project',async (req,res)=>{
+  const result=await uploadSchema.safeParse(req.body);
 
-const input={
+  if(result){
+    const {git_url,project_id}=req.body;
+    console.log(git_url)
+
+    const command=new RunTaskCommand({
       launchType:"FARGATE",
       cluster:"runix-cluster",
-      taskDefinition:'arn:aws:ecs:ap-south-1:977099018494:task-definition/runix-v2:2',
+      taskDefinition:'arn:aws:ecs:ap-south-1:977099018494:task-definition/runix-v2:4',
       overrides:{
         containerOverrides:[
         ],
         environment:[
-                {name:'GIT_REPOSITORY_URL',value:project_link},
-                {name:'PROJECT_ID',value:'p3'} 
+                {name:'GIT_REPOSITORY_URL',value: git_url},
+                {name:'PROJECT_ID',value: project_id }
         ]
       },
       networkConfiguration: { // NetworkConfiguration
@@ -40,20 +52,14 @@ const input={
           assignPublicIp: "ENABLED" ,
         },
       },
+    });
+    const response=await client.send(command);
 
-}
-
-app.post('/project',async(req,res)=>{
-    // const gitUrl=req.body;
-    console.log('req is here')
-    // const projectSlug=slugify(req.body);
-    const body=await req.body;
-    console.log(body);
-    // const command=new RunTaskCommand(input)
-    // const response = await client.send(command);
-
+  }
+  else{
+    res.json({message:"Error while parsing"})
+  }
 })
-
 
 app.listen(PORT,()=>{
     console.log(`Api server is running on ${PORT}`)
